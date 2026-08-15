806f73d1700b0533534470c73cf73b0a44a649dd
Aditya Modi <adityamodi108@gmail.com>
2026-08-15 22:41:54 +0530

    Time: 71 ms (74.20%) | Memory: 12.8 MB (20.31%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,26 @@
+from collections import Counter 
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def isapalindrome(l,r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        left = 0 
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return isapalindrome(left + 1, right) or isapalindrome(left , right - 1)
+
+            left += 1
+            right -= 1
+        return True 
+
+
